503691735e7b81ee4d970461a2f6e2e5167a4d01
Marcela DL <leon.ari00@hotmail.com>
2024-03-10 19:26:27 +0100

    Return temperature for any station and date

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template #web framewrk is a manager for multiple pages  
+from flask import Flask, render_template 
+import pandas as pd
 
-#Create a website object
+
 app = Flask(__name__)
 
-
-@app.route("/") #decorator that connects to .html file
+@app.route("/") 
 def home():
     return render_template("home.html")
 
 
 @app.route("/api/v1/<station>/<date>") 
 def api(station, date):
-    temperature = 23
+    filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE']== date]['   TG'].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
